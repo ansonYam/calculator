d43ba4f4d334263c2e2b617cba2205aa7e87b446
ansonYam <y.anson@hotmail.com>
2022-08-07 14:44:55 -0700

    Implement additional features
    Add floating point numbers, some CSS stylings, backspace button, keyboard support

--- script.js
+++ script.js
@@ -7,50 +7,75 @@ numberButtons.forEach((numberButton) => {
     numberButton.addEventListener('click', () => {
         display.innerText += numberButton.innerText;
     })
-});
+})
+
+// decimal event listener. Should only be allowed once
+const decimalButton = document.querySelector('#decimal');
+decimalButton.addEventListener('click', () => addDecimal());
+
+// keyboard support
+window.addEventListener('keydown', (event) => {
+    if (event.key >= 0 && event.key <= 9) display.innerText += event.key;
+    if (event.key === '.') addDecimal();
+    if (event.key === '=' || event.key === 'Enter') operatorFunction('equals');
+    if (event.key === 'Backspace') backSpace();
+    if (event.key === '+') operatorFunction('add');
+    if (event.key === '-') operatorFunction('subtract');
+    if (event.key === '*') operatorFunction('multiply');
+    if (event.key === '/') operatorFunction('divide');
+
+})
+
+// helper function for adding decimals
+function addDecimal() {
+    if (display.innerText.length >= 1 && !(display.innerText.includes('.'))) {
+        display.innerText += '.';
+    }
+}
+
+// helper function for deleting just one digit
+function backSpace() {
+    display.innerText = display.innerText.slice(0,-1);
+}
+
+function operatorFunction(chosenOperation) {
+    let displayValue = parseFloat(display.innerText); // lock in whatever is in the display (might be nothing)        
+    display.innerText = ''; // we've noted things down, can empty the display now 
+
+    if (isNaN(displayValue)) { // if we pressed an operation without any value, just update the operation and move on
+        queue.operation = chosenOperation;
+        queueDisplay.innerText = Math.round(queue.value * 1000)/1000 + ", " + queue.operation;
+        return;
+    }
+
+    if (chosenOperation === 'equals') {
+        if (!('operation' in queue)) {
+            queue.value = displayValue;
+        } else {
+            let newValue = operate(queue.operation, queue.value, displayValue); 
+            queue = []; // empty the queue
+            queue.value = newValue; // only push the new value, no operation here    
+        }
+        queueDisplay.innerText = Math.round(queue.value * 1000)/1000;
+        return;
+    }
+
+    if ('value' in queue && 'operation' in queue) {
+        let newValue = operate(queue.operation, queue.value, displayValue);
+        queue.value = newValue;    
+        queue.operation = chosenOperation;
+    } else {
+        queue.value = displayValue;
+        queue.operation = chosenOperation;
+    }    
+    queueDisplay.innerText = Math.round(queue.value * 1000)/1000 + ", " + queue.operation;
+}
 
 // pressing an operation button 'locks in' the number in the display
-const queueDisplay = document.querySelector('#queue'); // we can optionally show the queued value + operation
-
-// TODO: handle other 'out-of-order' sequences (how about starting with an operation and no value?)
+const queueDisplay = document.querySelector('#queue'); // we can show the queued value + operation
 const operatorButtons = document.querySelectorAll('.operator-btn');
 operatorButtons.forEach((operatorButton) => {
-    operatorButton.addEventListener('click', () => {
-        let displayValue = parseInt(display.innerText); // lock in whatever is in the display (might be nothing)        
-        let chosenOperation = operatorButton.id; // note down the chosen operation
-        display.innerText = ''; // we've noted things down, can empty the display now 
-
-        if (isNaN(displayValue)) { // if we pressed an operation without any value, just update the operation and move on
-            queue.operation = chosenOperation;
-            queueDisplay.innerText = Math.round(queue.value * 1000)/1000 + ", " + queue.operation;
-            return;
-        }
-
-        if (chosenOperation === 'equals') {
-            if (!('operation' in queue)) {
-                console.log('we are here!');
-                queue.value = displayValue;
-            } else {
-                let newValue = operate(queue.operation, queue.value, displayValue); 
-                queue = []; // empty the queue
-                queue.value = newValue; // only push the new value, no operation here    
-            }
-            queueDisplay.innerText = Math.round(queue.value * 1000)/1000;
-            return;
-        } 
-
-        if ('value' in queue && 'operation' in queue) {
-            let newValue = operate(queue.operation, queue.value, displayValue);
-            queue.value = newValue;    
-            queue.operation = chosenOperation;
-        } else {
-            console.log('pushing new value and new operation');
-            queue.value = displayValue;
-            queue.operation = chosenOperation;
-        }    
-        console.log(queue);
-        queueDisplay.innerText = Math.round(queue.value * 1000)/1000 + ", " + queue.operation;
-    })
+    operatorButton.addEventListener('click', (event) => operatorFunction(event.target.id));
 });
 
 const clearButton = document.querySelector('#clear');
@@ -58,7 +83,10 @@ clearButton.addEventListener('click', () => {
     queue = [];
     queueDisplay.innerText = queue.value + ", " + queue.operation;
     display.innerText = '';
-});
+})
+
+const deleteButton = document.querySelector('#delete');
+deleteButton.addEventListener('click', () => backSpace());
 
 function operate(operator, a, b) {
     switch(operator) {
